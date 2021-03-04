b592f98e2082498b3e4a8f966731e2279cdd22ca
waylandsingh <wmsingh@ucdavis.edu>
2021-03-04 11:54:55 -0800

    calculator and other jQuery added

---
+++ assignments/week7/keypress/new.js
@@ -0,0 +1,22 @@
+$(document).ready(function() {
+
+    console.log('something')
+    $("#target").keypress(function(event) {
+
+        // parse the input from ASCII
+        let keyPressed = String.fromCharCode(event.which);
+
+        // careful about comparison: which is a NUMBER ? probablyASCII for the letter
+        if (keyPressed == "+") {
+            let currentMarginTop = parseInt($("#target").css("margin-top"));
+
+            // console.log(currentMarginTop);
+
+            let newMargin = (currentMarginTop + 10) + "px"; //autoconcatenate as string
+
+            $("#target").css("margin-top", newMargin);
+        }
+        console.log("keyboard in use")
+    });
+
+});
